a0a394338464acd7030f32f7770a814207952e38
OchoOcho21 <ochoocho21162@gmail.com>
2025-07-01 16:50:42 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import { Providers } from '@/components/Providers';
 
 export const metadata: Metadata = {
-  title: 'OchoOcho21 | Aaron Ochoa Cloud',
+  title: 'AaronOchoa Cloud',
   description: 'Personal Cloud Storage',
   icons: {
     icon: '/favicon.ico',
